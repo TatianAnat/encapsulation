51897a972fb939b0936e5a590c3302e4cf6b8752
TatianAnat <TatianAnat@yandex.ru>
2025-06-08 20:31:04 +0400

    Encapsulation new1

--- src/com/skypro/JavaProfMain.java
+++ src/com/skypro/JavaProfMain.java
@@ -1,10 +1,13 @@
 package com.skypro;
 
+import com.skypro.account.v2.Account;
+
 public class JavaProfMain {
 
     public static void main(String[] args) {
         System.out.println("Skypro Uni - Java Profession start!");
         transferMoneyV1();
+        transferMoneyV2();
     }
 
     public  static void  transferMoneyV1() {
@@ -27,5 +30,28 @@ public class JavaProfMain {
 
         System.out.println(fromName + " имеет " + fromBalance);
         System.out.println(toName + " имеет " + toBalance);
+    }
+
+    public static void transferMoneyV2() {
+        System.out.println("JavaProfMain.transferMoneyV2");
+        Account ivan = new Account();
+        ivan.name = "Ivan";
+        ivan.balance = 100;
+        Account petr = new Account();
+        petr.name = "Petr";
+        petr.balance = 50;
+
+        int amount = 20;
+
+        System.out.println(ivan.name + " имеет " + ivan.balance);
+        System.out.println(petr.name + " имеет " + petr.balance);
+
+        System.out.println(ivan.name + " передает " + petr.name + " " + amount);
+
+        ivan.balance = ivan.balance - amount;
+        petr.balance = petr.balance + amount;
+
+        System.out.println(ivan.name + " имеет " + ivan.balance);
+        System.out.println(petr.name + " имеет " + petr.balance);
     }
 }
---
+++ src/com/skypro/account/v2/Account.java
@@ -0,0 +1,8 @@
+package com.skypro.account.v2;
+
+public class Account {
+
+    //указываем, что имя и баланс односится к одному человеку (сущности)
+    public String name;
+    public int balance;
+}
